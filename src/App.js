1df096cb50253d73377ec455f7acbf4107689347
Baiysh <115449261+kydyrmyshovbaiysh@users.noreply.github.com>
2022-10-11 17:04:44 +0600

    code

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Проект №1 <b>TO DO LIST</b>
+      </p>
+      <p>
+        1-й Разработчик <b>Jackie Chan</b>
+        <span> написал код авторизации</span>
+      </p>
+      <div>
+        <h1>Kyrgyzstan</h1>
+        <input type="range" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
